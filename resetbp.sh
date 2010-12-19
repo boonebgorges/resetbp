1215fb9e3cc8ad101f1b9421d74d70a2d7ce1565
Boone Gorges <boonebgorges@gmail.com>
2010-12-19 11:13:47 -0500

    First commit

---
+++ resetbp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+database=$1
+declare -a droptables=(wp_bp_activity wp_bp_activity_meta wp_bp_friends wp_bp_groups wp_bp_groups_groupmeta wp_bp_groups_members wp_bp_messages_messages wp_bp_messages_notices
+wp_bp_messages_recipients wp_bp_notifications wp_bp_user_blogs wp_bp_user_blogs_blogmeta  wp_bp_xprofile_data wp_bp_xprofile_fields wp_bp_xprofile_groups wp_bp_xprofile_meta wp_bb_forums wp_bb_meta wp_bb_posts wp_bb_terms wp_bb_term_relationships wp_bb_term_taxonomy wp_bb_topics)
+
+len=${#droptables[@]}
+for(( i=0; i<$len; i++))
+do
+	sql="USE $database;DROP TABLE ${droptables[$i]};"
+	mysql -u root --password=root <<< $sql
+done
+
+
+sql="USE $database;DELETE FROM wp_options WHERE option_name LIKE 'bp-%';"
+mysql -u root --password=root <<< $sql
+
+sql="USE $database;DELETE FROM wp_sitemeta WHERE meta_key LIKE 'bp-%';"
+mysql -u root --password=root <<< $sql
+
+#Switch back to Twenty Ten
+sql="USE $database;UPDATE wp_options SET option_value = 'twentyten' WHERE option_name = 'stylesheet' OR option_name = 'template';"
+mysql -u root --password=root <<< $sql
